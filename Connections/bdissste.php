6b0dc7d62eece076d035afd7722276c7befb3936
pablomagallon <pablomagallon@hotmail.com>
2020-07-21 18:41:04 -0500

    notas de evolución

---
+++ Connections/bdissste.php
@@ -0,0 +1,19 @@
+<?php
+# FileName="Connection_php_mysql.htm"
+# Type="MYSQL"
+# HTTP="true"
+$hostname_bdissste = "localhost";
+$database_bdissste = "sistema_censo";
+$username_bdissste = "root";
+$password_bdissste = "root";
+$database_bdisssteR = "sistema_agenducha";
+$username_bdisssteR = "root";
+$password_bdisssteR = "root";
+$database_bdisssteC = "sistema_cie10";
+$username_bdisssteC = "root";
+$password_bdisssteC = "root";
+$database_bdisssteF = "sistema_farmacia";
+$username_bdisssteF = "root";
+$password_bdisssteF = "root";
+
+?>
---
+++ Connections/bdisssteold.php
@@ -0,0 +1,12 @@
+<?php
+# FileName="Connection_php_mysql.htm"
+# Type="MYSQL"
+# HTTP="true"
+$hostname_bdissste = "localhost";
+$database_bdissste = "sistema_censo";
+$username_bdissste = "2667HRVGF";
+$password_bdissste = "S@sV4l3nt1n";
+$database_bdisssteR = "sistema_basedts";
+$username_bdisssteR = "2667HRVGF";
+$password_bdisssteR = "S@sV4l3nt1n";
+?>
